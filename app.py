33cd2a8f9710118035211615b1189de6b6c3272d
NotMine <129601022+Random0exe@users.noreply.github.com>
2023-12-16 16:25:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def index():
     return render_template('index.html', weapon=weapon, shield=shield, bullets=bullets)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
